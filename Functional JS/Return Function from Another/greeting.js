8e437a621b986d9e36adaeda68ec39c7d25f0a18
SohanR <mizan.rahman66d@gmail.com>
2020-04-18 13:43:18 +0600

    2 way for ret func

---
+++ Functional JS/Return Function from Another/greeting.js
@@ -0,0 +1,18 @@
+function greeting(msg) {
+  return function (name) {
+    console.log(msg + " " + name);
+  };
+}
+
+function greeting(msg) {
+  function ret(name) {
+    console.log(msg + " " + name);
+  }
+  return ret;
+}
+
+var good = greeting("Good morning");
+var hello = greeting("Hello");
+
+good("Sohan");
+hello("Sohan");
